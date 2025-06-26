c65b91f9001fc33a6214cb0b5800cab8b8d9c047
buntman <148082762+buntman@users.noreply.github.com>
2025-06-26 16:37:25 +0800

    fix conflict

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class AuthController extends Controller
+{
+    public function login():View {
+        return view('login');
+    }
+}
