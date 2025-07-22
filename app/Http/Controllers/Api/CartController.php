221ea907f6f8d67ed46c01c3cde6876bbde51411
buntman <148082762+buntman@users.noreply.github.com>
2025-07-23 00:11:14 +0800

    feat(api): implement api cart features(update,remove,view) - Implemented display of items in cart - Allows removing of item in cart - Update quantity functionality - Validations of product existence - Authenticates user actions

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -3,40 +3,40 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Product;
 use App\Models\Cart;
 use App\Models\CartItem;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\Models\Product;
 
 class CartController extends Controller
 {
-    public function addToCart($product_id) {
-        $product = Product::find($product_id);
-        if (!$product) {
-            return response()->json(['message' => 'Product does not exist!'], 422);
-        }
+    public function addToCart(Request $request) {
+        $input = $request->validate([
+            'product_id' => 'required|exists:products,id|integer',
+        ]);
+
+        $product = Product::find($input['product_id']);
 
         $user_id = auth('api')->id();
 
         $cart = Cart::where('user_id', $user_id)->where('status', 'active')->first();
         // Create a new cart for the user and add the selected product as the first cart item
         if (!$cart) {
-            Cart::create([
+            $new_cart = Cart::create([
                 'user_id' => $user_id,
                 'total_price' => $product->price,
             ]);
-            $cart = Cart::where('user_id', $user_id)->where('status', 'active')->first();
             CartItem::create([
-                'cart_id' => $cart->id,
-                'product_id' => $product_id,
+                'cart_id' => $new_cart->id,
+                'product_id' => $product->id,
                 'price' => $product->price,
                 'sub_total' => $product->price,
             ]);
             return response()->json(['message' => 'Added Successfully!'], 200);
         }
-
         //product already exists in user's cart
-        $cart_item = CartItem::where('product_id', $product_id)->where('cart_id', $cart->id)->first();
+        $cart_item = CartItem::where('product_id', $product->id)->where('cart_id', $cart->id)->first();
         if ($cart_item) {
             $cart_item->increment('quantity');
             $cart_item->update([
@@ -44,14 +44,80 @@ class CartController extends Controller
             ]);
             return response()->json(['message' => 'Added Successfully!'], 200);
         }
-
         //add new product to cart
         CartItem::create([
             'cart_id' => $cart->id,
-            'product_id' => $product_id,
+            'product_id' => $product->id,
             'price' => $product->price,
             'sub_total' => $product->price,
         ]);
         return response()->json(['message' => 'Added Successfully!'], 200);
+    }
+
+    public function fetchItemsInCart() {
+        $user_id = auth('api')->id();
+        $items = DB::table('carts')
+        ->join('cart_items', 'carts.id', '=', 'cart_items.cart_id')
+        ->join('products', 'cart_items.product_id', '=', 'products.id')
+        ->select('cart_items.id', 'products.image_name', 'products.name', 'cart_items.quantity', 'products.price')
+        ->where('carts.user_id', $user_id)
+        ->where('carts.status', 'active')
+        ->get();
+        foreach ($items as &$item) {
+            $item->image_name = 'http://10.0.2.2:8000/images/' . $item->image_name;
+        }
+        unset($item);
+        return response()->json($items, 200);
+    }
+
+    public function deleteItem($item_id) {
+        $item = CartItem::find($item_id);
+        if (!$item) {
+            return response()->json(['message' => 'Item does not exist.'], 404);
+        }
+        $cart = Cart::find($item->cart_id);
+        if ($cart->user_id != auth('api')->id()) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+        $item->delete();
+        //if no remaining items in cart, delete the cart
+        $remaining_item = CartItem::where('cart_id', $item->cart_id)->first();
+        if (!$remaining_item) {
+            Cart::where('id', $item->cart_id)->delete();
+        }
+        return response()->json(['message' => 'Removed from cart successfully.'], 200);
+    }
+
+    public function updateItemQuantity(Request $request, $item_id) {
+        $item = CartItem::find($item_id);
+        if (!$item) {
+            return response()->json(['message' => 'Item does not exist.'], 404);
+        }
+
+        $cart = Cart::find($item->cart_id);
+        if ($cart->user_id != auth('api')->id()) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $input = $request->validate([
+            'quantity' => 'required|min:1|integer',
+        ]);
+        $sub_total = $input['quantity'] * $item->price;
+        $item->update([
+            'quantity' => $input['quantity'],
+            'sub_total' => $sub_total,
+        ]);
+        return response()->noContent();
+    }
+
+    public function calculateTotalPrice() {
+        $user_id = auth('api')->id();
+        $cart = Cart::where('user_id', $user_id)->where('status', 'active')->first();
+        if (!$cart) {
+            return response()->json(['total_price' => 0], 200);
+        }
+        $items = CartItem::where('cart_id', $cart->id)->get();
+        $total_price = $items->sum('sub_total');
+        return response()->json(['total_price' => $total_price], 200);
     }
 }
