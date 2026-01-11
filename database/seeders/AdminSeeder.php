ba075edcbee086308972483c7b5a280e1736f13c
buntman <148082762+buntman@users.noreply.github.com>
2026-01-11 10:33:03 +0800

    create a seeder for admin account

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Admin::create([
+            'username' => env('ADMIN_USERNAME'),
+            'password' => Hash::make(env('ADMIN_PASSWORD')),
+        ]);
+    }
+}
